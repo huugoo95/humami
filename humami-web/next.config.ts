bc1b46438a33334d43912d79b4638c97df324094
hugo <huugoo95@gmail.com>
2025-05-02 10:19:25 +0200

    add index page

--- humami-web/next.config.ts
+++ humami-web/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// Asegúrate de que process.env esté disponible
+const imageDomain = process.env.NEXT_PUBLIC_IMAGE_DOMAIN || 'localhost'
 
+const nextConfig: NextConfig = {
+  images: {
+    domains: [imageDomain],
+  },
+}
 export default nextConfig;
